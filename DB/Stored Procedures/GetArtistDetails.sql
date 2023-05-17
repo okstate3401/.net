859faa4c8245cc939a4bbd74949ec911a4ea4690
Diego <okstate3401@gmail.com>
2023-05-16 23:44:46 -0500

    connected db, implemented logic for artist details page

---
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE dbo.GetArtistDetails
+	@artistId INT = 1
+AS
+BEGIN
+
+	SELECT ar.heroURL [artistHeroUrl], ar.imageURL [artistImageUrl], ar.title [artistTitle], ar.biography [artistBiography], 
+	al.albumID, al.imageURL [albumImageUrl], al.title [albumTitle], s.title [songTitle], s.bpm [songBPM], s.timeSignature [songTimeSignature]
+	FROM Artist ar
+	INNER JOIN Album al ON al.artistID = ar.artistID
+	INNER JOIN Song s ON s.albumID = al.albumID AND s.artistID = ar.artistID
+	WHERE ar.artistID = @artistId
+
+END
---
+++ Web/multitracks.com/multitracks.com/artistDetails.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataAccess;
+
+public partial class artistDetails : MultitracksPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        var sql = new SQL();
+
+        try
+        {
+            sql.Parameters.Add("@artistId", 5);
+            var data = sql.ExecuteStoredProcedureDT("GetArtistDetails");
+
+            songs.DataSource = data;
+            songs.DataBind();
+
+            // Select only unique albums - query could return duplicates if there are multiple songs in an album
+            DataTable dtAlbums = data.Rows.Cast<DataRow>().GroupBy(r => r.Field<int>("albumID"), (key, group) => group.First()).CopyToDataTable();
+            albums.DataSource = dtAlbums;
+            albums.DataBind();
+
+            // We only need the first row to fill this information
+            DataTable dtFirstRow = data.Clone();
+            dtFirstRow.ImportRow(data.FirstOrNewRow());
+            artistHeaderInfo.DataSource = dtFirstRow;
+            artistHeaderInfo.DataBind();
+
+            artistBiography.DataSource = dtFirstRow;
+            artistBiography.DataBind();
+        }
+        catch
+        {
+
+        }
+    }
+}
